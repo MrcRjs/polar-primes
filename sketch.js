f70e233aaf5be691c512f403a270d64541a92de9
Marco A. Rojas <marco.rojas@alumno.buap.mx>
2019-10-13 03:15:10 -0400

    Simulate prime points generation and keep canvas centered

--- sketch.js
+++ sketch.js
@@ -1,54 +1,79 @@
+let cnv;
+
+const VEL = 100;
+const Primes = sieve(1000000);
+const PL = Primes.length;
+const primePoints = new Array(PL);
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(0);
-	translate(width / 2, height / 2);
+    cnv = createCanvas(windowWidth, windowHeight);
+    background(0);
+    colorMode(HSB, 100);
+    noSmooth();
+	for(let i = 0; i < PL; i++)
+	{
+		const p = getCartesian(Primes[i], Primes[i]);
+		primePoints[i] = { x: p.x / 300, y: p.y/300};
+	}
+}
 
-	colorMode(HSB, 100);
+let prime_i = 0;
 
-	sieve(1000000).forEach((p, i) => {
-		let point = getCartesian(p, p);
-		ellipseMode(CENTER);
-		noStroke();
-		fill(
-			map(noise(i / 10000), 0, 1, 0, 100),
-			80,
-			100);
-		ellipse(point.x / 300, point.y / 300, 1, 1);
-	});
+function draw() {
+    centerCanvas();
+    if (prime_i < PL) {
+		for (let i=0; i < prime_i; i++) {
+			stroke(
+				map(noise(i / 10000), 0, 1, 0, 100), 80, 100);
+			point(primePoints[i].x, primePoints[i].y);
+		}
+        prime_i += VEL;
+    } else {
+    	noLoop();
+	}
+
 }
 
 function getCartesian(r, theta) {
-	let x = r * cos(theta);
-	let y = r * sin(theta);
-	return {x, y};
+    let x = r * cos(theta);
+    let y = r * sin(theta);
+    return {x, y};
 }
 
 function sieve(limit) {
 
-	var bools = [];
-	var primes = [];
+    var bools = [];
+    var primes = [];
 
-	for (let i = 1; i < limit; i++) {
-		bools.push(true);
-	}
+    for (let i = 1; i < limit; i++) {
+        bools.push(true);
+    }
 
-	for (let i = 2; i < limit; i++) {
-		if (bools[i - 2]) {
-			for (let j = i * 2; j <= limit; j += i) {
-				bools[j - 2] = false;
-			}
-		}
-	}
+    for (let i = 2; i < limit; i++) {
+        if (bools[i - 2]) {
+            for (let j = i * 2; j <= limit; j += i) {
+                bools[j - 2] = false;
+            }
+        }
+    }
 
-	for (let p = 0; p < bools.length; p++) {
-		if (bools[p]) {
-			primes.push(p + 2);
-		}
-	}
+    for (let p = 0; p < bools.length; p++) {
+        if (bools[p]) {
+            primes.push(p + 2);
+        }
+    }
 
-	return primes;
-} 
+    return primes;
+}
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+    centerCanvas();
+    prime_i-=VEL;
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function centerCanvas() {
+    const x = (cnv.width) / 2;
+    const y = (cnv.height) / 2;
+    cnv.translate(x, y);
 }
